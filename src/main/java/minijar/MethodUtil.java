198cc959b26a2d02a33dba41c492cf818d4e9ba3
Julian Dolby <dolby@us.ibm.com>
2021-10-12 14:16:12 -0400

    hacks

--- src/main/java/minijar/MethodUtil.java
+++ src/main/java/minijar/MethodUtil.java
@@ -20,27 +20,32 @@ import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.ipa.cha.IClassHierarchy;
 
 public class MethodUtil {
-    public static Set<String> getSuperMethods(IMethod m, IClassHierarchy cha) {
-        Set<String> ret = new HashSet<String>();
+    public static Set<IClass> getSuperClasses(IMethod m, IClassHierarchy cha) {
         Set<IClass> superClasses = new HashSet<IClass>();
         IClass superClass = m.getDeclaringClass().getSuperclass();
-        
+
         while(superClass != null) {
-          superClasses.add(superClass);
-          superClass = superClass.getSuperclass();
+            superClasses.add(superClass);
+            superClass = superClass.getSuperclass();
         }
-    
+
+        return superClasses;
+    }
+
+    public static Set<String> getSuperMethods(IMethod m, IClassHierarchy cha) {
+        Set<String> ret = new HashSet<String>();
+
         MethodDescriptor desc = new MethodDescriptor(m);
     
-        for(IClass klass: superClasses) {
-          String superMethod = getMethod(klass, desc.getNameType());
+        for(IClass klass: getSuperClasses(m, cha)) {
+          String superMethod = getMethodDesc(klass, desc.getNameType());
           if (superMethod != null) {
             ret.add(superMethod);
           }
         }
     
         for(IClass klass: m.getDeclaringClass().getAllImplementedInterfaces()){
-          String superMethod = getMethod(klass, desc.getNameType());
+          String superMethod = getMethodDesc(klass, desc.getNameType());
           if (superMethod != null) {
             ret.add(superMethod);
           }
@@ -48,13 +53,23 @@ public class MethodUtil {
     
         return ret;
       }
-    
-      public static String getMethod(IClass klass, String methodName) {
+
+    public static String getMethodDesc(IClass klass, String methodName) {
+        IMethod m  = getMethod(klass, methodName);
+        if (m != null) {
+            MethodDescriptor desc = new MethodDescriptor(m);
+            return desc.toString();
+        } else {
+            return null;
+        }
+    }
+
+    public static IMethod getMethod(IClass klass, String methodName) {
         for (IMethod m: klass.getAllMethods()) {
-          MethodDescriptor desc = new MethodDescriptor(m);
+            MethodDescriptor desc = new MethodDescriptor(m);
           String superMethodName = desc.getNameType();
           if (superMethodName.equals(methodName)) {
-            return desc.toString();
+            return m;
           }
         }
         return null;
